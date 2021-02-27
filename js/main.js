1a3909df2ba256a5dae93c21c176d515b8b11c63
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2021-02-27 10:01:48 +0600

    Header Complete with responsive and now coppy it .....

--- js/main.js
+++ js/main.js
@@ -2,4 +2,28 @@ $('documnet').ready(function() {
 
   new WOW().init(); /*wow start*/
   
+   $('.navbar-toggler').sidr({
+        name: 'asidr-main',
+        side: 'right',
+        source: '#asidr',
+        displace: false,
+        renaming: false,
+    });
+
+    $('.sidr-close').on('click', function(){
+        $.sidr('close', 'asidr-main');
+    });
+
+    $('body').swipe( {
+        //Single swipe handler for left swipes
+        swipeLeft: function () {
+            $.sidr('open', 'asidr-main');
+        },
+        swipeRight: function () {
+            $.sidr('close', 'asidr-main');
+        },
+        //Default is 75px, set to 0 for demo so any distance triggers swipe
+        threshold: 45
+    });
+    
 });
