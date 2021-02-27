cce0c566e39847cc68f54fca7fb7fd9dbadbf442
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2021-02-27 22:52:47 +0600

    Complete Project and Header copy for you

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ $('documnet').ready(function() {
   new WOW().init(); /*wow start*/
   
    $('.navbar-toggler').sidr({
-        name: 'asidr-main',
         side: 'right',
-        source: '#asidr',
-        displace: false,
         renaming: false,
+        displace: false,
+        source: '#asidr',
+        name: 'asidr-main',
     });
 
     $('.sidr-close').on('click', function(){
@@ -16,14 +16,42 @@ $('documnet').ready(function() {
 
     $('body').swipe( {
         //Single swipe handler for left swipes
-        swipeLeft: function () {
-            $.sidr('open', 'asidr-main');
-        },
         swipeRight: function () {
             $.sidr('close', 'asidr-main');
+            $('.show-overly').removeClass('active');
         },
+        // swipeLeft: function () {
+        //   $('.show-overly').removeClass('active');
+        // },
+        swipeLeft: function () {
+            $.sidr('open', 'asidr-main');
+            $('.show-overly').addClass('active')
+        },
+        /* swipeRight: function () {
+            $('.show-overly').addClass('active');
+          },*/
         //Default is 75px, set to 0 for demo so any distance triggers swipe
         threshold: 45
+    }); 
+
+    $('.navbar-toggler').on('click',function(){
+        $('.show-overly').addClass('active');
+    });
+
+    $('.sidr-close').on('click',function(){
+        $('.show-overly').removeClass('active');
+    });
+
+    $('.search-triger').on('click',function(){
+        $('.search-bar').addClass('active');
+    });
+
+    $('.search-close').on('click',function(){
+        $('.search-bar, .show-overly').removeClass('active');
+    });
+
+    $('.search-triger').on('click',function(){
+        $('.show-overly').addClass('active');
     });
     
 });
