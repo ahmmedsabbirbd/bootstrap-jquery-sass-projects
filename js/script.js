290914e6b9c10fe6bc4ff6eedaf4e3cde5dc725e
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2021-05-10 17:02:49 +0600

    Search page compleate

--- js/script.js
+++ js/script.js
@@ -46,4 +46,51 @@ $('documnet').ready(function() {
     window.onload = setGutterHeight;
     window.onresize = setGutterHeight;
 
+    /*/// init Isotope ///*/
+    var $grid = $('.product-filter').isotope({
+        itemSelector: '.item'
+    });
+
+    var filters = {};    /// store filter for each group
+
+    $('.product-filter-button').on( 'click', '.button', function( event ) {
+        var $button = $( event.currentTarget );
+        var $buttonGroup = $button.parents('.filter-items');
+        var filterGroup = $buttonGroup.attr('data-filter-group');
+        filters[ filterGroup ] = $button.attr('data-filter');
+        var filterValue = concatValues( filters );
+        $grid.isotope({ filter: filterValue });
+    });
+
+    
+    $('.filter-items').each( function( i, buttonGroup ) {
+        var $buttonGroup = $( buttonGroup );
+
+        $buttonGroup.on( 'click', 'button', function( event ) {
+        $buttonGroup.find('.active').removeClass('active');
+            var $button = $( event.currentTarget );
+            $button.addClass('active');
+        });
+    }); /// change is-checked class on buttons
+
+
+    
+    function concatValues( obj ) {
+        var value = '';
+        for ( var prop in obj ) {
+          value += obj[ prop ];
+        }
+        return value;
+    }  // flatten object by concatting values
+ 
+    $('.product-gallery .product-item').each( function() { $(this).hoverdir(); } );
+
+    // jQuery(function(){
+    //     jQuery("#design-video").YTPlayer({
+    //         mute: true,
+    //         opacity: .6,
+    //         autoplay: true,
+    //         showControls: false,
+    //     });
+    // });
 });
